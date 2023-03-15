6f4615ea373edd5f26d0fc96413329b29203777f
Trudy <gcyn51a@gmail.com>
2023-03-15 12:11:10 +0300

    Update Whitelist Contract Address

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x8f8FAAC99deCdd871dBA65e84411C15928d12f18";
+const WHITELIST_CONTRACT_ADDRESS = "0xf353Ddd692f08B2D9B948B44e87407a6FB30F004";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-ten-lemon.vercel.app/api/";
 
